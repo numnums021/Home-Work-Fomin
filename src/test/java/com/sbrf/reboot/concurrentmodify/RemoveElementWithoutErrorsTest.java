f2c975c53ba84218aab99c35bae004b870f72e97
danya <numnums021@gmail.com>
2022-01-27 15:29:14 +0300

    Реализованы методы, избегающие исключение ConcurrentModificationException: - successRemoveElementUsingRemoveIf() - удаляет элемент из списка с помощью removeIf(); - successDeleteItemUsingStreams() - удаляет элемент из списка с помощью Streams; - successRemoveElementWithNewList() - удаляет элемент из списка с помощью нового списка.
    Необходимо добиться правильного выполнения теста с Streams. Произошёл взлом мозга, так как [2, 3] не равно [2, 3] после обработки Streams.

--- src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
+++ src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
@@ -1,15 +1,24 @@
 package com.sbrf.reboot.concurrentmodify;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.ConcurrentModificationException;
-import java.util.List;
+import java.util.*;
 
+import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+/**
+ * Класс тестирования для обработки исключения ConcurrentModificationException.
+ * @autor Фомин Даниил (numnums021)
+ * @version 1.8
+ */
 public class RemoveElementWithoutErrorsTest {
 
+    /**
+     * Процедура проверки вызова исключения при удалении из ArrayList
+     * элемента с индексом '1'.
+     */
     @Test
     public void successConcurrentModificationException() {
 
@@ -20,17 +29,68 @@ public class RemoveElementWithoutErrorsTest {
         }};
 
         assertThrows(ConcurrentModificationException.class, () -> {
-            
             for (Integer integer : list) {
                 list.remove(1);
             }
-            
         });
 
     }
 
+    /**
+     * Процедура проверки правильного удаления элемента из ArrayList
+     * с помощью removeIf().
+     */
     @Test
-    public void successRemoveElementWithoutErrors() {
+    public void successRemoveElementUsingRemoveIf() {
+
+        List<Integer> list = new ArrayList(Arrays.asList(1, 2, 3));
+
+        List<Integer> checkList = new ArrayList(Arrays.asList(2, 3));
+
+        list.removeIf(i -> i == 1);
+
+        Assertions.assertEquals(list, checkList);
+
+    }
+
+    /**
+     * Процедура проверки правильного удаления элемента из ArrayList
+     * с помощью Streams.
+     */
+    @Test
+    public void successDeleteItemUsingStreams() {
+
+        List<Integer> list = new ArrayList(Arrays.asList(1, 2, 3));
+
+        List<Integer> checkList = new ArrayList(Arrays.asList(2, 3));
+
+        List<String> newList = list.stream()
+                .filter(i -> i != 1)
+                .map(Object::toString) // Object::toString - ссылка на метод
+                .collect(toList());
+
+        Assertions.assertTrue(checkList.containsAll(newList));
+
+    }
+
+    /**
+     * Процедура проверки правильного удаления элемента из ArrayList
+     * с помощью 'решения в лоб' посредством переноса в новый ArrayList
+     */
+    @Test
+    public void successRemoveElementWithNewList() {
+
+        List<Integer> list = new ArrayList(Arrays.asList(1, 2, 3));
+
+        List<Integer> checkList = new ArrayList(Arrays.asList(2, 3));
+
+        List<Integer> newList = new ArrayList();
+
+        for (Integer item: list)
+            if (item != 1)
+                newList.add(item);
+
+        Assertions.assertEquals(newList, checkList);
 
     }
 
